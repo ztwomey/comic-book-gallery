5d544ce5b7bbbd2f322fd143f398f82090e1b0d6
ztwomey <zrt685@hotmail.com>
2018-03-07 20:25:33 -0500

    Added Detail Action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,8 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail() {
+            return "Hello from the comic books controller!";
+        }
     }
 }
